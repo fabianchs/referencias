5971d4128f85831e2c38e3663d5f8878008ec1cf
fabianchs <fabichasola@hotmail.com>
2021-06-04 19:18:04 +0000

    whatever

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,99 +7,254 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<p>
-				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
-				que pretendía indemnización por $13 millones ​. ameliarueda.com.
-				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
-					Recuperado de este enlace
-				</a>
-			</p>
-			<p>
-				Banco Mundial. (2021). PIB (US$ a precios actuales).
-				Datos.bancomundial.org. Recuperado el 30 de Mayo del 2021,
-				<a href="https://datos.bancomundial.org/indicador/NY.GDP.MKTP.CD?end=2002&locations=PE&name_desc=false&start=1997 ">
-					Recuperado de este enlace
-				</a>
-			</p>
-			<p>
-				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
-				que pretendía indemnización por $13 millones ​. ameliarueda.com.
-				<a href="https://datos.bancomundial.org/indicator/NY.GDP.PETR.RT.ZS?end=2001&locations=PE&start=1997 ">
-					Recuperado de este enlace
-				</a>
-			</p>
-			<p>
-				Banco Mundial. (2021). Rentas del petróleo (% del PIB).
-				Datos.bancomundial.org. Recuperado el 30 de Mayo del 2021
-				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
-					Recuperado de este enlace
-				</a>
-			</p>
-			<p>
-				Congreso del Gobierno peruano. (2006). Www2.congreso.gob.pe.
-				Recuperado el 30 de Mayo del 2021
-				<a href="https://www2.congreso.gob.pe/Sicr/Prensa/heraldo.nsf/04NoticicasArchivoHistorico/1d3be5442b1eb3aa052571a9000dde8f/?OpenDocument ">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				Cordero Araya, P. (2011). Análisis Jurídico del Caso Harken
-				Costa Rica Holdings: Bases para una posible defensa del Estado
-				de Costa Rica ante una demanda internacional. [PDF] (p. 41).
-				Recuperado el 30 de Mayo del 2021
-				<a href="https://iij.ucr.ac.cr/wp-content/uploads/bsk-pdf-manager/2017/06/Analisis-Juridico-del-Caso-Harken-Costa-Rica-Holdings.pdf.">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				Editor Summa. (2011, 9 septiembre). Petrolera Mallon Oil acude a
-				Tribunales costarricenses. Revista Summa.
-				<a href="https://revistasumma.com/16604/">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				Garza, J. (2020, 16 junio). En Costa Rica sí hay petróleo, según
-				Colegio de Geólogos. LaRepublica
-				<a href="https://www.larepublica.net/noticia/en-costa-rica-si-hay-petroleo-segun-colegio-de-geologos">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				Informa-Tico.com. (2014, 1 agosto). Gobierno dictó moratoria a
-				explotación petrolera. Informa-Tico.
-				<a href="https://www.informa-tico.com/1-08-2011/gobierno-dicto-moratoria-explotacion-petrolera ">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
-				que pretendía indemnización por $13 millones ​. ameliarueda.com.
-				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
-				que pretendía indemnización por $13 millones ​. ameliarueda.com.
-				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
-				que pretendía indemnización por $13 millones ​. ameliarueda.com.
-				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
-					Recuperado de este enlace
-				</a>
-			</p>{" "}
-			<p>
-				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
-				que pretendía indemnización por $13 millones ​. ameliarueda.com.
-				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
-					Recuperado de este enlace
-				</a>
-			</p>
+			<div className="row d-flex justify-content-center">
+				<div className="col-xl-6 col-lg-8 col-md-10 col-sm-12">
+					<p>
+						AR.com. (2014, 17 diciembre). Costa Rica gana juicio a
+						petrolera que pretendía indemnización por $13 millones
+						ameliarueda.com.
+						<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Banco Mundial. (2021). PIB (US$ a precios actuales).
+						Datos.bancomundial.org. Recuperado el 30 de Mayo del
+						2021,
+						<a href="https://datos.bancomundial.org/indicador/NY.GDP.MKTP.CD?end=2002&locations=PE&name_desc=false&start=1997 ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						AR.com. (2014, 17 diciembre). Costa Rica gana juicio a
+						petrolera que pretendía indemnización por $13 millones.
+						ameliarueda.com.
+						<a href="https://datos.bancomundial.org/indicator/NY.GDP.PETR.RT.ZS?end=2001&locations=PE&start=1997 ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Banco Mundial. (2021). Rentas del petróleo (% del PIB).
+						Datos.bancomundial.org. Recuperado el 30 de Mayo del
+						2021
+						<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Congreso del Gobierno peruano. (2006).
+						Www2.congreso.gob.pe. Recuperado el 30 de Mayo del 2021
+						<a href="https://www2.congreso.gob.pe/Sicr/Prensa/heraldo.nsf/04NoticicasArchivoHistorico/1d3be5442b1eb3aa052571a9000dde8f/?OpenDocument ">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Cordero Araya, P. (2011). Análisis Jurídico del Caso
+						Harken Costa Rica Holdings: Bases para una posible
+						defensa del Estado de Costa Rica ante una demanda
+						internacional. [PDF] (p. 41). Recuperado el 30 de Mayo
+						del 2021
+						<a href="https://iij.ucr.ac.cr/wp-content/uploads/bsk-pdf-manager/2017/06/Analisis-Juridico-del-Caso-Harken-Costa-Rica-Holdings.pdf.">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Editor Summa. (2011, 9 septiembre). Petrolera Mallon Oil
+						acude a Tribunales costarricenses. Revista Summa.
+						<a href="https://revistasumma.com/16604/">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Garza, J. (2020, 16 junio). En Costa Rica sí hay
+						petróleo, según Colegio de Geólogos. LaRepublica
+						<a href="https://www.larepublica.net/noticia/en-costa-rica-si-hay-petroleo-segun-colegio-de-geologos">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Informa-Tico.com. (2014, 1 agosto). Gobierno dictó
+						moratoria a explotación petrolera. Informa-Tico.
+						<a href="https://www.informa-tico.com/1-08-2011/gobierno-dicto-moratoria-explotacion-petrolera ">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Minae. (2012). Plan remedial de gestión ambiental.
+						Recuperado el 2 de Junio del 2021
+						<a href="https://minae.go.cr/ ">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						AR.com. (2014, 17 diciembre). Costa Rica gana juicio a
+						petrolera que pretendía indemnización por $13 millones.
+						ameliarueda.com.
+						<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Los tres pilares de la sostenibilidad. miResiduo.
+						(2019). Recuperado el 30 de Mayo del 2021,
+						<a href="https://meuresiduo.com/es/blog-es/comprender-los-tres-pilares-de-la-sostenibilidad/ ">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Perúpetro. (1998). Estadística de Hidrocarburos [PDF]
+						(1era ed., p. 73). Perúpetro. Recuperado el 30 de Mayo
+						del 2021
+						<a href="https://www.perupetro.com.pe/wps/wcm/connect/corporativo/9e172f40-6b94-41f7-ac6b-5848911b2fa9/Estadistica+1998.pdf?MOD=AJPERES&1998 ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Perúpetro. (2000). Estadística de Hidrocarburos [PDF]
+						(1era ed., pp. 18,74). Perúpetro. Recuperado el 30 de
+						Mayo del 2021
+						<a href="https://www.perupetro.com.pe/wps/wcm/connect/corporativo/e5155860-7010-4e16-bba5-464e667c56c1/Estadistica+2000.pdf?MOD=AJPERES&2000 ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Perúpetro. (2003). Estadística Petrolera [PDF] (1era
+						ed., p. 65). Perúpetro. Recuperado el 20 de Mayo del
+						2021,
+						<a href="https://www.perupetro.com.pe/wps/wcm/connect/corporativo/d99e3388-f33f-47d5-b3bc-db0f32423e5d/Estadistica+2003.pdf?MOD=AJPERES&2003 ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Perúpetro. (2021). Estadística Petrolera.
+						Perupetro.com.pe. Recuperado el 20 de Mayo del 2021,
+						<a href="https://www.perupetro.com.pe/wps/portal/corporativo/PerupetroSite/estadisticas/estad%C3%ADstica%20petrolera/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zi_YxcTTw8TAy93AN8LQwCTUJcvEKADEMfE_1wsAIDHMDRQD8Kt_5AIzOofjwWROE3Pji1GGhGFCFlXvpR6Tn5SRAfOeYlGVuk60cVpaalFqUW6ZUWAYUzSkoKiq1UDVQNysvL9dLz89NzUvWS83NVDbBpycgvLtGPQFWpX5AbGlERkBtuUeyoCACrSmaB/dz/d5/L2dBISEvZ0FBIS9nQSEh/ ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Rueda, A. (2014). Costa Rica gana juicio a petrolera que
+						pretendía indemnización por $13 millones ​.
+						ameliarueda.com. Recuperado el 30 de Mayo del 2021,
+						<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo.">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Sequeira, A. (2017, 30 octubre). Luis Guillermo Solís
+						extiende moratoria a explotación petrolera hasta el
+						2021. La Nacion.Recuperado el 30 de Mayo del 2021,
+						<a href="https://www.nacion.com/el-pais/politica/luis-guillermo-solis-extiende-moratoria-a-explotacion-petrolera-hasta-el-2021/WJ6DUCWVXVHD7J4WLKKORJRBTE/story/ ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Virginia Cajiao, M. (2002). Las concesiones petroleras
+						en el Caribe Costarricense [PDF] (1st ed., pp. 17,18).
+						IPECA. Recuperado el 30 de Mayo del 2021,
+						<a href="https://www.elaw.org/system/files/costa.rica.petroleras.pdf ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Uribe, B., Spiewak, I., {"&"} Soria, A. (1999). Precios
+						del petróleo más bajos: implicaciones a largo plazo.
+						Recuperado el 30 de Mayo del 2021,
+						<a href="https://vlex.es/vid/petroleo-bajos-implicaciones-largo-111274 ">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Ministerio de Hacienda de Costa Rica. (s.f). Sobre Costa
+						Rica. Recuperado el 30 de Mayo del 2021,
+						<a href="https://www.hacienda.go.cr/contenido/12542-sobre-costa-rica">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Martínez Ramón, C. (2016). Los efectos de cambios en el
+						precio del petróleo sobre las cuentas públicas de los
+						países exportadores de petróleo. Recuperado el 30 de
+						Mayo del 2021
+						<a href="https://repositorio.upct.es/handle/10317/5857">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Astorga, A. (2019). Opinion: La falacia de la riqueza
+						petrolera de Costa Rica. Recuperado el 30 de Mayo del
+						2021
+						<a href="https://www.crhoy.com/opinion/el-lector-opina/opinion-la-falacia-de-la-riqueza-petrolera-de-costa-rica/">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Salcedo, J. D. (2016) La experiencia en el mundo de
+						países exportadores de petróleo. Recuperado el 30 de
+						Mayo del 2021
+						<a href="https://www.researchgate.net/profile/Juan_Salcedo/publication/332393543_La_experiencia_en_el_mundo_de_paises_exportadores_de_petroleo/links/5cb13ec5a6fdcc1d49910a11/L">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Bravo, E. (2007). Los impactos de la explotación
+						petrolera en ecosistemas tropicales y la biodiversidad.
+						Acción ecológica, 24(1), 35-42. Recuperado el 30 de Mayo
+						del 2021
+						<a href="https://www.academia.edu/download/46944247/impactos_explotacion_petrolera_esp.pdf">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Miranda, D., {"&"} Restrepo, R. (2005). Los derrames de
+						petróleo en ecosistemas tropicales–impactos,
+						consecuencias y prevención. La experiencia de Colombia.
+						In International oil spill conference (Vol. 2005, No. 1,
+						pp. 571-575). American Petroleum Institute. Recuperado
+						el 30 de Mayo del 2021
+						<a href="https://meridian.allenpress.com/iosc/article/2005/1/571/138794">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Bravo-Oilwatch, E. (2005). Explotación petrolera en la
+						Reserva de la Biosfera Yasuní-Ecuador. Asalto al
+						Paraíso, 36. Recuperado el 30 de Mayo del 2021, de
+						<a href="https://www.accionecologica.org/wp-content/uploads/Asalto_al_paraiso-copia.pdf#page=37">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Pacheco, A. (2002). Mensaje Inaugural de la toma de
+						posesión del presidente de la república de Costa Rica,
+						el Dr. Abel Pacheco de la Espriella, periodo
+						constitucional Mayo 2002-Mayo 2006. Recuperado el 18 de
+						Mayo del 2021,
+						<a
+							href="http://www.oilwatchsudamerica.org/doc/declaracion/decla2002_johanmora-esp.pdf
+">
+							Recuperado de este enlace
+						</a>
+					</p>{" "}
+					<p>
+						Rodríguez Echavarría, T. (2016). Petróleo y resistencia
+						en Centroamérica. El caso de Acción de lucha
+						antipetrolera (ADELA) en el Caribe sur de Costa Rica
+						(1999-2002). Recuperado el 21 de Mayo del 2021,
+						<a href="http://repositorio.ucr.ac.cr/bitstream/handle/10669/74076/30235-87833-1-PB%20%281%29.pdf?sequence=1&isAllowed=y">
+							Recuperado de este enlace
+						</a>
+					</p>
+					<p>
+						Universidad de Costa Rica. (2021). Compensación
+						ambiental: un debate pendiente. Universidad de Costa
+						Rica. Retrieved 4 June 2021,
+						<a href="https://www.ucr.ac.cr/noticias/2021/02/10/compensacion-ambiental-un-debate-pendiente.html.">
+							Recuperado de este enlace
+						</a>
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
