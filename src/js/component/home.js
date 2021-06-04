d80669110b3508f5cfb4c27d8704e026e67c2cca
fabianchs <fabichasola@hotmail.com>
2021-06-04 17:21:42 +0000

    whatever

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,17 +7,96 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
 			<p>
-				<img src={rigoImage} />
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
 			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				Banco Mundial. (2021). PIB (US$ a precios actuales).
+				Datos.bancomundial.org. Recuperado el 30 de Mayo del 2021,
+				<a href="https://datos.bancomundial.org/indicador/NY.GDP.MKTP.CD?end=2002&locations=PE&name_desc=false&start=1997 ">
+					Recuperado de este enlace
+				</a>
+			</p>
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>
+			<p>
+				Banco Mundial. (2021). Rentas del petróleo (% del PIB).
+				Datos.bancomundial.org. Recuperado el 30 de Mayo del 2021
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
+			</p>{" "}
+			<p>
+				AR.com. (2014, 17 diciembre). Costa Rica gana juicio a petrolera
+				que pretendía indemnización por $13 millones ​. ameliarueda.com.
+				<a href="https://www.ameliarueda.com/nota/costa-rica-gana-juicio-a-petrolera-que-pretendia-indemnizacion-por-13-millo">
+					Recuperado de este enlace
+				</a>
 			</p>
 		</div>
 	);
